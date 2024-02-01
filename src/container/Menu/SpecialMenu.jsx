968c8f26ca983813010186264dd3f67c5b8a39c6
HuniaNad <nadeemsonaina987@gmail.com>
2024-02-01 07:32:43 +0500

    feat: added MenuItem component's jsx

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -17,7 +17,7 @@ const SpecialMenu = () => {
           <p className='app__specialMenu-menu_heading'>Wine & Bear</p>
           <div className="app__specialMenu-menu_items">
             {data.wines.map((wine, index) => (
-              <p>{wine.title}</p>
+              <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
             ))}
           </div>
         </div>
@@ -30,7 +30,7 @@ const SpecialMenu = () => {
           <p className='app__specialMenu-menu_heading'>Wine & Bear</p>
           <div className="app__specialMenu-menu_items">
             {data.cocktails.map((cocktail, index) => (
-              <p>{cocktail.title}</p>
+              <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price}  tags={cocktail.tags} />
             ))}
           </div>
         </div>
